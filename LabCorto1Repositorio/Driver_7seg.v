1255243cf1210ebb29a6cbcdb09834ced20c6962
BettieBetz <betz.93@gmail.com>
2015-02-28 20:57:15 -0600

    Update Driver_7seg.v

--- LabCorto1Repositorio/Driver_7seg.v
+++ LabCorto1Repositorio/Driver_7seg.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Tecnológico de Costa Rica
+// Engineer: Kaled Alfaro e Irene Rivera
 // 
 // Create Date:    18:56:38 02/23/2015 
 // Design Name: 
